52ef481fca60068120b7fd0bb965da3c950b0725
nickherrig <neherrig@gmail.com>
2022-04-10 20:37:41 -0500

    Add styles, image, and additional tutorial info

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,16 @@
-import { useState } from 'react'
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
 
-function Header({ title }) {
-  return <h1>{title ? title : 'Default title'}</h1>
-}
-
-export default function HomePage() {
-  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton', 'test']
-  const [likes, setLikes] = useState(0)
-
-  function handleClick() {
-    setLikes(likes - 2)
-  }
-
+export default function Home() {
   return (
-    <div>
-      <Header title="Develop. Preview. Ship. 🚀" />
-      <ul>
-        {names.map(name => (
-          <li key={name}>{name}</li>
-        ))}
-      </ul>
-
-      <button onClick={handleClick}>Like ({likes})</button>
-    </div>
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>I build things with code</p>
+      </section>
+    </Layout>
   )
 }
